b9d40641919b5528aca6cce345e8197eb953019d
fernando992013 <99453563+fernando992013@users.noreply.github.com>
2023-01-30 22:34:29 -0500

    Add files via upload

---
+++ Coco-Cola-Free-Style-App-main/Form2.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form2
+    Public flavor As String
+    Private Sub picCola2_Click(sender As Object, e As EventArgs) Handles picCola2.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coco Cola"
+    End Sub
+
+    Private Sub picColaCreme_Click(sender As Object, e As EventArgs) Handles picColaCreme.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Creme"
+    End Sub
+
+    Private Sub picColaCherry_Click(sender As Object, e As EventArgs) Handles picColaCherry.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Cherry"
+    End Sub
+
+    Private Sub picColaCherryVanilla_Click(sender As Object, e As EventArgs) Handles picColaCherryVanilla.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Cherry Vanilla"
+    End Sub
+
+    Private Sub picColaLemon_Click(sender As Object, e As EventArgs) Handles picColaLemon.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Lemon"
+    End Sub
+
+    Private Sub picColaLime_Click(sender As Object, e As EventArgs) Handles picColaLime.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Lime"
+    End Sub
+
+    Private Sub picColaOrange_Click(sender As Object, e As EventArgs) Handles picColaOrange.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Orange"
+    End Sub
+
+    Private Sub picColaRaspberry_Click(sender As Object, e As EventArgs) Handles picColaRaspberry.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Raspberry"
+    End Sub
+
+    Private Sub picColaVanilla_Click(sender As Object, e As EventArgs) Handles picColaVanilla.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Vanilla"
+    End Sub
+
+    Private Sub picColaOrangeVanilla_Click(sender As Object, e As EventArgs) Handles picColaOrangeVanilla.Click
+        Form1.Show()
+        Form1.Enabled = True
+        Me.Close()
+        flavor = "Coca Cola Orange Vanilla"
+    End Sub
+End Class
